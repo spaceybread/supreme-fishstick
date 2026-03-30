9ae1f6cdf3f711531b51ab17314ed56f7d408e18
spaceybread <spaceybread@gmail.com>
2026-03-30 16:39:04 -0500

    fixed env

--- make_env.py
+++ make_env.py
@@ -1,7 +1,6 @@
 from gym import spaces
 from gym import Env
 import numpy as np
-import clear_output
 from make_maze import Maze
 
 import random
@@ -22,15 +21,19 @@ class MazeEnv(Env):
         self.maze_size = 10
         self.grid_size = 2 * self.maze_size + 1
 
-        self.observation_space = spaces.Box(0, 9, [self.grid_size * self.grid_size], dtype=np.int16)
+        self.observation_space = self.observation_space = spaces.Box(0, 1, shape=(8,), dtype=np.float32)
         self.action_space = spaces.Discrete(4)
 
         self.player_pos = [1, 1]
         self.target_pos = [2 * self.maze_size - 1, 2 * self.maze_size - 1]
+        
+        self.max_steps = 1000
+        self.current_steps = 0
 
         self.reset()
         
     def step(self, action):
+        self.current_steps += 1
         moves = {UP: (-2, 0), DOWN: (2, 0), LEFT: (0, -2), RIGHT: (0, 2)}
         wall_offsets = {UP: (-1, 0), DOWN: (1, 0), LEFT: (0, -1), RIGHT: (0, 1)}
 
@@ -47,15 +50,53 @@ class MazeEnv(Env):
             self.grid[new_r * self.grid_size + new_c] = PLAYER
             self.player_pos = [new_r, new_c]
 
+        tr, tc = self.target_pos
+        pr, pc = self.player_pos
+        
         done = (self.player_pos == self.target_pos)
-        reward = 1.0 if done else -0.01
-        return self.grid.copy(), reward, done, {}
+        
+        max_dist = ((self.grid_size**2 + self.grid_size**2) ** 0.5)
+        dist = ((pr - tr)**2 + (pc - tc)**2) ** 0.5
+        reward = -(dist / max_dist)
+
+        if (pr, pc) in self.visited_cells: reward -= 0.3
+        else:
+            self.visited_cells.add((pr, pc))
+            reward += 0.1
+
+        if done: reward = 10.0
+        
+        truncated = self.current_steps >= self.max_steps
+        done = done or truncated
+        
+        return self.get_obs(), reward, done, {}
+        
+    def get_obs(self):
+        pr, pc = self.player_pos
+        tr, tc = self.target_pos
+        r, c = self.player_pos
+
+        wall_up    = 1 if self.grid[(r-1)*self.grid_size + c] == WALL else 0
+        wall_down  = 1 if self.grid[(r+1)*self.grid_size + c] == WALL else 0
+        wall_left  = 1 if self.grid[r*self.grid_size + (c-1)] == WALL else 0
+        wall_right = 1 if self.grid[r*self.grid_size + (c+1)] == WALL else 0
+
+        return np.array([
+            pr / self.grid_size,
+            pc / self.grid_size,
+            tr / self.grid_size,
+            tc / self.grid_size,
+            wall_up, wall_down, wall_left, wall_right
+        ], dtype=np.float32)
     
     def reset(self):
+        self.current_steps = 0
+        self.visited_cells = set()
+        self.visited_cells.add((1, 1))
         maze = Maze(self.maze_size)
         self.grid = np.array(maze.flatten(), dtype=np.int16)
         self.player_pos = [1, 1]
-        return self.grid.copy()
+        return self.get_obs()
     
     def render(self):
         ma = {0: ' ', 9: '#', 1: 'o', 2: 'x'}
